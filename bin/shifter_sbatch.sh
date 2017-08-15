2d14812bd3a1b7ac091507e3b7eabd9a83da5d97
Lee James O'Riordan <loriordan@gmail.com>
2017-08-15 15:51:27 -0700

    Shifter SLURM submission scripts

---
+++ bin/shifter_sbatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --partition=<partition>
+#SBATCH --job-name=cxid9114_index
+#SBATCH --time=<walltime>
+#SBATCH --nodes=<nnodes>
+#SBATCH --constraint=haswell
+#SBATCH --image=docker:mlxd/xfel:lq79
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=loriordan@lbl.gov
+#SBATCH --volume="/global/cscratch1/sd/mlxd/DataProc/cxid9114/cxi:/reg/d/psdm/CXI;/global/cscratch1/sd/mlxd/DataProc/cxid9114/cxi:/reg/d/psdm/cxi;/global/cscratch1/sd/mlxd/DataProc/cxid9114/regg/g:/reg/g"
+
+# submit jobs
+srun -n <nproc> shifter <srun_script>
---
+++ bin/shifter_srun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#for experiment database
+export SIT_DATA=/reg/g/psdm/data
+
+#for psana
+export SIT_PSDM_DATA=/reg/d/psdm
+
+#cctbx
+source /build/setpaths.sh
+
+# run
+<command>
