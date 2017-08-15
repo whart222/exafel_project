237135bd4e3809ed66652a375d7290a515c154a8
Lee James O'Riordan <loriordan@gmail.com>
2017-08-15 16:07:35 -0700

    Added averaging submission script

---
+++ bin/averaging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --partition=regular
+#SBATCH --job-name=cxid9114_mask
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --image=docker:mlxd/xfel:lq79
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=loriordan@lbl.gov
+#SBATCH --volume="/global/cscratch1/sd/mlxd/DataProc/cxid9114/cxi:/reg/d/psdm/CXI;/global/cscratch1/sd/mlxd/DataProc/cxid9114/cxi:/reg/d/psdm/cxi;/global/cscratch1/sd/mlxd/DataProc/cxid9114/regg/g:/reg/g"
+
+if [ ! -f ./avg.sh ]; then 
+    echo "#\!/bin/bash" >> ./avg.sh
+    echo "export SIT_DATA=/reg/g/psdm/data"  >> ./avg.sh
+    echo "export SIT_DATA=/reg/d/psdm"  >> ./avg.sh
+    echo "source /build/setpaths.sh"  >> ./avg.sh
+    echo "cxi.mpi_average -x cxid9114 -r \${1} -a CxiDs2.0:Cspad.0 -d 572 -v -g 6.85" >> ./avg.sh
+fi
+
+# submit jobs
+srun -n 68 -c 1 --cpu_bind=cores shifter ./avg.sh ${1} -R
+srun -n 68 -c 1 --cpu_bind=cores shifter ./avg.sh ${2}
