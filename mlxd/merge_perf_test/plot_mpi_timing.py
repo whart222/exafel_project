932cb1e320bd1b136f60e5ba052996619262bb34
Lee J. O'Riordan <loriordan@gmail.com>
2018-01-10 14:51:42 -0800

    Generalised start and end file strings

--- mlxd/merge_perf_test/plot_mpi_timing.py
+++ mlxd/merge_perf_test/plot_mpi_timing.py
@@ -3,8 +3,11 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-start = np.loadtxt(open(str(sys.argv[1])), delimiter=',', dtype={'names': ('A','B','t'), 'formats':('i4','i4','f8')})
-end = np.loadtxt(open(str(sys.argv[2])), delimiter=',', dtype={'names': ('A','B','t'), 'formats':('i4','i4','f8')})
+StartStr = str(sys.argv[1])
+EndStr = str(sys.argv[2])
+
+start = np.loadtxt(open(StartStr), delimiter=',', dtype={'names': ('A','B','t'), 'formats':('i4','i4','f8')})
+end = np.loadtxt(open(EndStr), delimiter=',', dtype={'names': ('A','B','t'), 'formats':('i4','i4','f8')})
 
 ds=[{'%s:%s'%(a,b): (a,b,t) for a,b,t in zip(start['A'],start['B'],start['t']) }]
 de=[{'%s:%s'%(a,b): (a,b,t) for a,b,t in zip(end['A'],end['B'],end['t']) }]
@@ -25,7 +28,7 @@ for a in ds[0].keys():
 ax.set_zlim3d([ 0, np.max(end['t']) - t0 ])
 ax.set_ylim3d([ np.min([end['A'], end['B']]), np.max([end['A'],end['B']]) ])
 ax.set_xlim3d([ np.min([end['A'], end['B']]), np.max([end['A'],end['B']]) ])
-plt.savefig('3d_%s_%s.pdf'%(str(sys.argv[1]), str(sys.argv[2])))
+plt.savefig('3d_%s_%s.pdf'%(StartStr, EndStr))
 plt.clf()
 
 #2D connections diagram
@@ -38,5 +41,5 @@ for ii in xrange( len(start['A']) ):
     	plt.axhline(ii, xmin=0, xmax=1)
 plt.xlabel('time [t]')
 plt.ylabel('MPI rank')
-plt.title('%s_%s'%(str(sys.argv[1]), str(sys.argv[2])))
-plt.savefig('2d_%s_%s.pdf'%(str(sys.argv[1]), str(sys.argv[2])))
+plt.title('%s_%s'%(StartStr, EndStr))
+plt.savefig('2d_%s_%s.pdf'%(StartStr, EndStr))
