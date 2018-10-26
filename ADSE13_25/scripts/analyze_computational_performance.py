39e95fe72f8f2ae123c5661706518181131f1454
Asmit <asmit3@gmail.com>
2018-10-25 18:17:32 -0700

    Weather plot analysis of MPI ranks for xtc_process
    This tool analyzes the timestamps in the debug files printed out by xtc_process and plots at what point in
    the processing a frame was processed and by which rank. Gives a sense of network issues, MPI performance bottlenecks
    Use --help for more info
    
    Co-authored-by: Asmit Bhowmick <asmit3@gmail.com>
    Co-authored-by: Aaron Brewster <asbrewster@lbl.gov>
    Co-authored-by: Nicholas Sauter <nksauter@lbl.gov>

---
+++ ADSE13_25/scripts/analyze_computational_performance.py
@@ -0,0 +1,82 @@
+from __future__ import print_function, division
+import matplotlib.pyplot as plt
+import sys,os
+from iotbx.detectors.cspad_detector_formats import reverse_timestamp
+from libtbx.phil import parse
+from libtbx.utils import Sorry
+
+message = ''' script to get a sense of the computational performance of every rank while processing xtc streams
+              End product is a plot of wall time vs MPI rank number with every data point being that of a frame
+              processed by xtc_process. The information is read in from the debug files created by xtc_process
+              Example usage on cxic0415 processed demo data -
+              libtbx.python analyze_computational_performance.py
+              input_path=/global/project/projectdirs/lcls/mona/output_demo18/cxic0415/output/debug
+'''
+phil_scope = parse('''
+  input_path = .
+    .type = str
+    .help = path to where the debug txt files are located
+  num_nodes = 32
+    .type = int
+    .help = Number of nodes used to do data processing. Used in timing information
+  num_cores_per_node = 68
+    .type = int
+    .help = Number of cores per node in the machine (default is for Cori KNL)
+  wall_time = 3600
+    .type = int
+    .help = total wall time (seconds) taken for job to finish. Used for plotting node-partitioning
+''')
+
+def params_from_phil(args):
+  user_phil = []
+  for arg in args:
+    if os.path.isfile(arg):
+      user_phil.append(parse(file_name=arg))
+    else:
+      try:
+        user_phil.append(parse(arg))
+      except Exception as e:
+        raise Sorry("Unrecognized argument: %s"%arg)
+  params = phil_scope.fetch(sources=user_phil).extract()
+  return params
+
+def run(params):
+  counter = 0
+  reference = None
+  root=params.input_path
+  for filename in os.listdir(root):
+    if os.path.splitext(filename)[1] != '.txt': continue
+    if 'debug' not in filename: continue
+    timepoints = []
+    rank = int(filename.split('_')[1].split('.')[0])
+    counter += 1
+    print (filename)
+    for line in open(os.path.join(root,filename)):
+      hostname, psanats, ts, status, result = line.strip().split(',')
+      if reference is None:
+        sec, ms = reverse_timestamp(ts)
+        reference = sec+ms*1e-3
+
+      if status in ['stop','done']:
+        sec, ms = reverse_timestamp(ts)
+        timepoints.append((sec + ms*1.e-3)-reference)
+        ok = True
+      else:
+        ok = False
+    plt.plot(timepoints, [rank]*len(timepoints), 'b.')
+    if not ok:
+      sec, ms = reverse_timestamp(ts)
+      plt.plot([(sec+ms*1e-3) - reference], [rank], 'rx')
+    #if counter > 100: break
+  for i in range(params.num_nodes):
+    plt.plot([0,params.wall_time], [i*params.num_cores_per_node-0.5, i*params.num_cores_per_node-0.5], 'r-')
+  plt.xlabel('Wall time (sec)')
+  plt.ylabel('MPI Rank Number')
+  plt.show()
+
+if __name__ == '__main__':
+  if '--help' in sys.argv[1:] or '-h' in sys.argv[1:]:
+    print (message)
+    exit()
+  params = params_from_phil(sys.argv[1:])
+  run(params)
